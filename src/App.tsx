08ca06ba7e06677fae57f832042779bd86ac9eda
Andrew Parkin <andrew.r.parkin@gmail.com>
2023-03-06 20:09:01 +1100

    feature: :sparkles: style Joyride directly

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,16 @@ const App = () => {
   const state = {
     steps: [
       {
-        target: '.click-button',
+        target: '.tour-step1',
         content: 'This is my awesome feature!',
       },
       {
-        target: '.other-click-button',
+        target: '.tour-step2',
         content: 'This another awesome feature!',
+      },
+      {
+        target: '.tour-step3',
+        content: 'Jam packed with awesome features!',
       },
     ]
   };
@@ -21,12 +25,26 @@ const App = () => {
   return (
     <div className="App">
       <Box>
-        <Button variant="contained" className="click-button">Click Me NOW</Button>
-        <Button variant="contained" className="other-click-button">And then Click Me!</Button>
+        <Button variant="contained" className="tour-step1">Click Me NOW</Button>
+        <Button variant="contained" className="tour-step3">And then Click Me!</Button>
+        <Button variant="contained" className="tour-step2">Who dares clicks...</Button>
       </Box>
       <Joyride
         steps={steps}
-        beaconComponent={Beacon as React.ElementType<BeaconRenderProps>}
+        // beaconComponent={Beacon as React.ElementType<BeaconRenderProps>}
+        styles={{
+          options: {
+            arrowColor: 'grey',
+            backgroundColor: 'grey',
+            primaryColor: 'purple',
+            textColor: 'white',
+            zIndex: 1000,
+          }
+        }}
+        continuous
+        disableOverlay
+        showProgress
+        showSkipButton
       />
     </div>
   );
